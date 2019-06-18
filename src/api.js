26089d31222d0c281abaf4834a5192480f7efb11
ascotttoney <ascotttoney@gmail.com>
2019-06-18 10:57:48 -0400

    Tuesday morning

---
+++ src/api.js
@@ -0,0 +1,38 @@
+// --- RECIPES --- //
+
+function fetchRecipes() {
+  return fetch('http://localhost:3000/recipes')
+  .then(resp => resp.json())
+}
+
+
+// --- USERS --- //
+
+function fetchUsers() {
+  return fetch('http://localhost:3000/users')
+  .then(resp => resp.json())
+}
+
+
+// --- REVIEWS --- //
+
+function fetchReviews() {
+  return fetch('http://localhost:3000/reviews')
+  .then(resp => resp.json())
+}
+
+
+// --- INGREDIENTS --- //
+
+function fetchIngredients() {
+  return fetch('http://localhost:3000/ingredients')
+  .then(resp => resp.json())
+}
+
+
+// --- RECIPE INGREDIENTS --- //
+
+function fetchRecipeIngredients() {
+  return fetch('http://localhost:3000/recipe_ingredients')
+  .then(resp => resp.json())
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  const baseURL = 'http://localhost:3000'
+  const cardGallery = document.getElementById('card-gallery')
 
   getRecipes()
 
-
+  
+// --- GET FUNCTIONS --- //
 
   function getRecipes() {
-    fetch('http://localhost:3000/recipes')
-    .then(resp => resp.json())
-    .then(json => console.log(json))
+    fetchRecipes()
+    .then(json => json.data.forEach(showRecipes))
   }
+
+
+// --- SHOW FUNCTIONS --- //
+
+  function showRecipes(recipes) {
+    console.log(recipes)
+
+    const recipeCardDiv = document.createElement('div')
+    recipeCardDiv.id = 'recipe-card'
+
+    const nameH1 = document.createElement('h1')
+    nameH1.innerText = recipes.attributes.name
+
+    cardGallery.append(recipeCardDiv, nameH1)
+  }
+
 
 // --- END --- //
 
